b229d3fe87aabfcd05e02d9706e0b03d020ee9c6
AnushaG001 <anushag1001@gmail.com>
2022-09-26 22:19:28 +0530

    third commit

---
+++ frequencycounting.py
@@ -0,0 +1,13 @@
+def CountFreq(i):
+    freq = 0
+    for item in li:
+        if (item % i == 0):
+            freq += 1
+    #print(freq) 
+    return freq
+li =[1,2,8,9,12,46,76,82,15,20,30]
+numstr = {}
+for i in range (1,9):
+    numstr[i] = CountFreq(i) 
+
+print(numstr)
